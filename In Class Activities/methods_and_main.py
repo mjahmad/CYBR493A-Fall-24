816fae7c45afdeeb6cd9bce921bba2af87738766
MJ Ahmad <mahmad2@mix.wvu.edu>
2024-09-19 09:11:16 -0400

    Modified methods_and_main for HW1 help.

--- In Class Activities/methods_and_main.py
+++ In Class Activities/methods_and_main.py
@@ -2,23 +2,30 @@
 Examples of using ping in python
 """
 import os
+import time
+from datetime import datetime
+
 def method_name(parameter):
     result = parameter + 1
     return result
+
+def ping_something(ip):
+    ping_command = "ping " + ip
+    ping_result = os.system(ping_command)
+    return ping_result
 
 
 def main():
     """
     Main function to prompt for an IP address and check if the host is responding.
     """
-    value_to_pass = int(input("Enter a value: "))
-    method_call = method_name(value_to_pass)
+    current_time  = (datetime.now())
 
-    if method_call == 0:
-        print(f"{method_call} is zero.")
-    else:
-        print(f"{method_call} is not zero.")
+    result_from_method = ping_something("127.0.0.1")
+    print(result_from_method)
+
 
 
 if __name__ == "__main__":
+    print("Hello World!")
     main()
